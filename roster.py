e37b394f642e18bf59632ae7b014ab5f1dc984fb
Neil Shah <neshah@unc.edu>
2025-11-12 14:55:23 -0500

    expanded dictionary

--- roster.py
+++ roster.py
@@ -1,6 +1,11 @@
 #https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
 
 roster = ["Bacot", "Davis", "High"]
-for player in roster:
-    print(player)
+player = {"Last Name": roster,
+          "First Name": ["Armando", "RJ", "Zayden"],
+          "height": [83, 72, 73],
+          "weight": [240,180,180]}
+data = pd.DataFrame(player)
+print(data)
 
